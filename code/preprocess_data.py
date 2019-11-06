350c250c69c576acce21a619e5a01e6bb20b8bc5
hainh <hainh@runsystem.net>
2019-11-06 13:42:28 +0700

    add thu vien

--- code/preprocess_data.py
+++ code/preprocess_data.py
@@ -2,11 +2,27 @@ from vncorenlp import VnCoreNLP
 import re
 import sys
 import os
+from underthesea import ner
 
 
-def detect_ner(list_senc):
+def detect_ner(list_senc, ners):
+	list_par=[]
 	for par in list_senc:
+		list_senc=[]
 		for senc in par:
+
+			##vncorenlp
+			# PERSON = ners.ner(senc)
+			# for p in PERSON[0]:
+			# 	if p[1] != 'O':
+			# 		senc = senc.replace(p[0].replace('_', ' '), p[1])
+
+			#underthesea
+			PERSON = ner(senc)
+			for p in PERSON:
+				if p[3] != 'O':
+					senc = senc.replace(p[0], p[3])
+
 
 			DATE = re.findall(r"\d\d*[-|/]\d\d*[-|/]\d\d\d\d|[N|n]gày \d\d* tháng \d\d* năm \d\d\d\d|[N|n]ăm \d\d\d\d|[T|t]háng \d\d*", senc)
 			if DATE!=[]:
@@ -20,12 +36,12 @@ def detect_ner(list_senc):
 						if code in c:
 							senc = senc.replace(c,'CODE')
 
-			MONNEY = re.findall(r"\d*,\d*,\d*[,|.]*\d*[,|.]*\d*", senc)
-			if MONNEY!=[]:
-				for monney in MONNEY:
+			MONEY = re.findall(r"\d*,\d*,\d*[,|.]*\d*[,|.]*\d*", senc)
+			if MONEY!=[]:
+				for money in MONEY:
 					for m in senc.split(' '):
-						if monney in m:
-							senc = senc.replace(m, 'MONNEY')
+						if money in m:
+							senc = senc.replace(m, 'MONEY')
 
 			NUMBER = re.findall(r'\d\d*[,]*\đ*', senc)
 			if NUMBER!=[]:
@@ -34,32 +50,56 @@ def detect_ner(list_senc):
 						if number in n:
 							senc = senc.replace(n, 'NUMBER')
 
-			print(senc)
-			# if NUMBER!=[]:
-			# 	print(NUMBER)
-			# print(NUMBER)
+			list_senc.append(senc)
+		# print('. '.join(list_senc))
+		list_par.append('. '.join(list_senc))
+	return '\n'.join(list_par)
+
+
+def write_file(txt, name, output='../result/'):
+	with open(output+name, 'w+') as f:
+ 		f.write(txt)
 
 def remove_space(content):
-	return '\n'.join([' '.join(line.strip().split()) for line in content.split('\n') if line.strip() != ''])
+	list_s = [' '.join(line.strip().split()) for line in content.split('\n') if line.strip() != '']
+	return '\n'.join([s.lower() if s.isupper() else s for s in list_s])
 	
 
-def process(content, path_jar_file='./VnCoreNLP-1.1.1.jar'):
+def process(content, ner, name):
 	non_space = remove_space(content)
 	paragraph = non_space.split('\n')
 	sentence = map(lambda s: [i_.strip() for i_ in s.split('.') if i_ != ''] ,paragraph)
 	list_senc = list(sentence)
-	detect_ner(list_senc)
-	# annotator = VnCoreNLP(path_jar_file, annotators="wseg,pos,ner,parse", max_heap_size='-Xmx2g')
-	# result = annotator.annotate(content)   
+	# write_file('\n'.join(['. '.join(_) for _ in list_senc]), 'data.txt')
+	# write_file(detect_ner(list_senc, ner), name)
+	gen_data_crnn()
+
 	# return result
 
+def gen_data_crnn():
+	words = set()
+	content = open('../result/data.txt', 'r').read()
+	for i in content.split('\n'):
+		for j in i.split(' '):
+			words.add(j)
 
-def main(folder_input):
+	with open('../result/words.txt', 'w+') as f:
+		for i in words:
+			f.write(i+'\n')
+
+
+
+def main(folder_input, path_jar_file='/home/gmo/Desktop/SpellingCrectionVN/VnCoreNLP/VnCoreNLP-1.1.1.jar'):
+
+	# ner = VnCoreNLP(path_jar_file, annotators="wseg,pos,ner,parse", max_heap_size='-Xmx2g')
+	ner=''
+	# print(ner.ner("Ông Nguyễn Khắc Chúc  đang làm việc tại Đại học Quốc gia Hà Nội. Bà Lan, vợ ông Chúc, cũng làm việc tại đây."))
+
 	for i in os.listdir(folder_input):
 		print(i+'--------------------------------')
 		content = open(folder_input+i, 'r').read()
 		# print(content)
-		result = process(content)
+		result = process(content, ner, i)
 
 
 if __name__ == '__main__':
